d9b695a30bf022a9e2482f378cc20174b3fbcd9b
ss0ryan0 <mattsorian0777@gmail.com>
2023-12-08 13:23:22 -0500

    trace1 12/8

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,17 @@
-const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise)
-d3.json(url).then(function(data) {
-    console.log(data);
+//d3 library read
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+const samplesData = d3.json(url)
+samplesData.then(function(data) {
+
 });
+    console.log(data);
+
+//Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+//using sample_values as the values, otu_ids as the labels, & otu_labels as the hovertext
+   let sortedOTU = samplesData.sort((a,b) => b.samples.sample_values - a.samples.sample_values);
+   let top10otu = sample_values.slice(0,10);
+   let trace1 = {
+        x: top10otu.map(d => d.samples.sample_values),
+        y: top10otu.map(d => 'OTU ${d.otu_ids}'),
+        text: top10otu.map(d => d.otu_labels)
+   }; 
