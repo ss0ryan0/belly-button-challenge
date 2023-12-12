a31ffa81fa692b405834d738dd8fbc957577fb8c
ss0ryan0 <mattsorian0777@gmail.com>
2023-12-12 17:29:30 -0500

    askBCS

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # belly-button-challenge
+#https://chat.openai.com/share/a1521e8a-00d1-4c18-887d-d20751510335
--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,10 @@
-//d3 library read
+//d3 library read: refer to class 12/ activity  14.3.1-2, 
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 const samplesData = d3.json(url)
-samplesData.then(function(data) {
-
+samplesData.then((data) => {
+    let metadata = data.metadata;
+    console.log(metadata)
 });
-    console.log(data);
 
 //Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 //using sample_values as the values, otu_ids as the labels, & otu_labels as the hovertext
@@ -13,5 +13,46 @@ samplesData.then(function(data) {
    let trace1 = {
         x: top10otu.map(d => d.samples.sample_values),
         y: top10otu.map(d => 'OTU ${d.otu_ids}'),
-        text: top10otu.map(d => d.otu_labels)
+        text: top10otu.map(d => d.samples.otu_labels)
    }; 
+
+   let barChart = [trace1];
+   let barLayout = {
+    title: "Top 10 OTUs",
+    margin: {
+        l: 200,
+        r: 200,
+        t: 200,
+        b: 200,
+    }
+   };
+   Plotly.newPlot("plot", barChart, barLayout);
+//Create a bubble chart that displays each sample.   
+   var trace2 = {
+    x: samples.otu_ids, //Use otu_ids for the x values.
+    y: samples.sample_values, //Use sample_values for the y values.
+    text: samples.otu_labels, //Use otu_labels for the text values.
+    mode: 'markers',
+    marker: {
+        size: samples.sample_values, //Use sample_values for the marker size.
+        color: samples.otu_ids //Use otu_ids for the marker colors.
+    }
+   };
+
+// Display the sample metadata, i.e., an individual's demographic information.
+const metadataDisplay = d3.select("#metadata");
+const selectedData = data.find(entry => entry.otu_id === selectedID);
+
+metadataDisplay.html(""); // Clear existing content
+
+//Display each key-value pair from the metadata JSON object somewhere on the page.
+for (const [key, value] of Object.entries(selectedData)) {
+    metadataDisplay.append("p").text(`${key}: ${value}`);
+}
+
+
+// Dropdown event listener
+d3.select("#dropdown").on("change", function() {
+const selectedID = d3.select(this).property("value");
+updatePlots(selectedID);
+});
